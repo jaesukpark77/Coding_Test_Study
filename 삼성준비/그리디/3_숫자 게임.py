44ab07f0b057d0068a7f691c02f3509300d298c8
JAESEOK PARK <jaesukpark77@naver.com>
2022-10-08 09:05:06 +0900

    그리디 연습 문제
    3_숫자 게임

---
+++ 삼성준비/그리디/3_숫자 게임.py
@@ -0,0 +1,22 @@
+# solve 1 => min()함수 사용
+n, m = map(int, input().split())
+
+result = 0
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = min(data)
+    result = max(result, min_value)
+print(result)
+
+# solve 2 => 2중 반복문
+n, m = map(int, input().split())
+
+result = 0
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = 10001
+    for a in data:
+        min_value = min(min_value, a)
+    result = max(result, min_value)
+
+print(result)
