7bf5fa5fe4a7482360ba0118c21956863d7e3767
JAESEOK PARK <jaesukpark77@naver.com>
2022-10-10 17:10:06 +0900

    다이나믹 프로그래밍 연습 문제
    피보나치 함수

---
+++ 삼성준비/다이나믹 프로그래밍/피보나치 함수.py
@@ -0,0 +1,31 @@
+# 재귀 함수로 구현
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
+    return fibo(x-1) + fibo(x-2)
+
+print(fibo(4))
+
+# 메모이제이션을 통한 피보나치 수옇
+d = [0] * 100
+
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    d[x] = fibo(x-1) + fibo(x-2)
+    return d[x]
+
+print(fibo(99))
+
+# DP 테이블 활용
+d = [0] * 100
+d[1] = 1
+d[2] = 1
+n = 99
+
+for i in range(3, n+1):
+    d[i] = d[i-1] + d[i-2]
+
+print(d[n])
