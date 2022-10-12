73c7fc93550165c98d682cfc1ce556768442e4f8
JAESEOK PARK <jaesukpark77@naver.com>
2022-10-12 23:41:50 +0900

    중요 내용 복습
    구현_게임 개발 문제

---
+++ 삼성준비/중요내용/구현_게임 개발.py
@@ -0,0 +1,48 @@
+n, m = map(int, input().split()) # n X m 맵 생성
+d = [[0] * m for _ in range(n)] # 방문한 위치 저장
+
+x, y, direction = map(int, input().split())
+d[x][y] = 1
+
+array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def turn_left():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
+
+# 시뮬레이션 시작
+count = 1
+turn_time = 0
+while True:
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+    else:
+        turn_time += 1
+
+    if turn_time == 4:
+        nx = x -dx[direction]
+        ny = y -dy[direction]
+        if d[nx][ny] == 0:
+            x = nx
+            y = ny
+        else:
+            break
+        turn_time = 0
+
+print(count)
