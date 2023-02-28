3680cb31378f2aef3d6f2fcb56b663db5c1a7862
Jaeseok Park <jaesukpark77@gmail.com>
2023-03-01 08:27:31 +0900

    Add. 그리드_1이 될 때까지 problem - solution 2

--- 재도전/1. 그리디/4, 1이 될 때까지.py
+++ 재도전/1. 그리디/4, 1이 될 때까지.py
@@ -13,4 +13,20 @@ while n > 1:
   n -= 1
   cnt += 1
 
-print(cnt)    
+print(cnt)
+
+# solution 2
+n, k = map(int, input().split())
+cnt = 0
+
+while True:
+  target = (n // k) * 4
+  cnt += (n - target)
+  n = target
+  if n < k:
+    break
+  cnt += 1
+  n //= k
+
+cnt += (n - 1)
+print(cnt)
