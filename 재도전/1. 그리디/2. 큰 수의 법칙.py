368454b90d314c49a4aa5cd75d7e009fcb86011e
Jaeseok Park <jaesukpark77@gmail.com>
2023-03-01 00:55:07 +0900

    Add. 그리드_큰 수의 법칙 problem - solution 2

--- 재도전/1. 그리디/2. 큰 수의 법칙.py
+++ 재도전/1. 그리디/2. 큰 수의 법칙.py
@@ -1,4 +1,5 @@
-n, m, k = map(int, input().split())
+# solution 1
+""" n, m, k = map(int, input().split())
 
 data = list(map(int, input().split()))
 
@@ -18,5 +19,20 @@ while True:
     break
   result += second
   m -= 1
+
+print(result) """
+
+# solution 2
+n, m, k = map(int, input().split())
+
+data = list(map(int, input().split()))
+
+data.sort(reverse=True)
+first = data[0]
+second = data[1]
+
+count = ((m//(k+1)) * k) + (m % (k+1))
+
+result = (count * first) + ((m-count) * second)
 
 print(result)
