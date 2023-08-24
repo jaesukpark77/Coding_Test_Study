c1b6ca7fcba467c7f96bc89ae4e0d4943b425f26
Jaeseok Park <jaesukpark77@gmail.com>
2023-08-24 15:30:32 +0900

    Add. codetree problem_완전탐색 Ⅱ - 독서실의 거리두기 5 problem

---
+++ codetree/완전탐색 Ⅲ/독서실의 거리두기 5.py
@@ -0,0 +1,23 @@
+# problem link : https://www.codetree.ai/missions/5/problems/study-cafe-keeping-distance-5?&utm_source=clipboard&utm_medium=text
+
+n = int(input())
+seat = list(input())
+
+def min_dist():
+    dist = n
+    for i in range(n):
+        for j in range(i+1, n):
+            if seat[i] == '1' and seat[j] == '1':
+                dist = min(dist, j - i)
+    
+    return dist
+
+ans = 0
+
+for i in range(n):
+    if seat[i] == '0':
+        seat[i] = '1'
+        ans = max(ans, min_dist())
+        seat[i] = '0'
+
+print(ans)
