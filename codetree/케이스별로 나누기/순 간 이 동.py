ea666565df6e8e1d97c19681b687875e32b6516f
Jaeseok Park <jaesukpark77@gmail.com>
2023-09-01 18:16:33 +0900

    Add. codetree problem_케이스별로 나누기 -순 간 이 동 problem

---
+++ codetree/케이스별로 나누기/순 간 이 동.py
@@ -0,0 +1,15 @@
+# problem link : https://www.codetree.ai/missions/5/problems/teleportation?&utm_source=clipboard&utm_medium=text
+
+import math
+
+INT_MAX = math.inf
+
+a, b, x, y = map(int, input().split())
+
+min_dist = INT_MAX
+
+min_dist = min(min_dist, abs(b - 1))
+min_dist = min(min_dist, abs(x - a) + abs(b - y))
+min_dist = min(min_dist, abs(y - a) + abs(b - x))
+
+print(min_dist)
