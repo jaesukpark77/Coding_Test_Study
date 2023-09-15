44b5e786e346065f6c785c6d9ce4f68dccd53653
Jaeseok Park <jaesukpark77@gmail.com>
2023-09-15 18:03:22 +0900

    Add. codetree problem_INTERMEDIATE LOW_01. Simulation_격자 안에서 밀고 당기기 - 삼각형 컨베이어 벨트 problem

---
+++ codetree/INTERMEDIATE LOW/01. Simulation/격자 안에서 밀고 당기기/삼각형 컨베이어 벨트.py
@@ -0,0 +1,32 @@
+# problem link : https://www.codetree.ai/missions/2/problems/conveyor-belt-triangle?&utm_source=clipboard&utm_medium=text
+
+n, t = map(int, input().split())
+l = list(map(int, input().split()))
+r = list(map(int, input().split()))
+d = list(map(int, input().split()))
+
+for _ in range(t):
+    temp = l[n - 1]
+    for i in range(n - 1, 0, -1):
+        l[i] = l[i - 1]
+    l[0] = d[n - 1]
+    
+    temp2 = r[n - 1]
+    for i in range(n - 1, 0, -1):
+        r[i] = r[i - 1]
+    r[0] = temp
+
+    for i in range(n - 1, 0, -1):
+        d[i] = d[i - 1]
+    d[0] = temp2
+    
+for elem in l:
+    print(elem, end=" ")
+print()
+
+for elem in r:
+    print(elem, end=" ")
+print()
+
+for elem in d:
+    print(elem, end=" ")
