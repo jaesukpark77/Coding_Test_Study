72ccc62e8fe187a53c4d2682b6f898247017ca48
Jaeseok Park <jaesukpark77@gmail.com>
2023-09-21 23:05:21 +0900

    Add. codetree problem_INTERMEDIATE LOW_02. Backtracking_순열 만들기 - 외판원 순회 problem

---
+++ codetree/INTERMEDIATE LOW/02. Backtracking/순열 만들기/외판원 순회.py
@@ -0,0 +1,50 @@
+# problem link : https://www.codetree.ai/missions/2/problems/traveling-salesman-problem?&utm_source=clipboard&utm_medium=text
+
+import math
+
+INT_MAX = math.inf
+
+n = int(input())
+cost = [list(map(int, input().split())) for _ in range(n)]
+visited = [False] * n
+picked = []
+
+ans = INT_MAX
+
+def find_min(cnt):
+    global ans
+
+    if cnt == n:
+        total_cost = 0
+        for i in range(n -1):
+            curr_cost = cost[picked[i]][picked[i+1]]
+
+            if curr_cost == 0:
+                return
+
+            total_cost += curr_cost
+
+        additional_cost = cost[picked[-1]][0]
+        if additional_cost == 0:
+            return
+
+        ans = min(ans, total_cost + additional_cost)
+        return
+
+    for i in range(n):
+        if visited[i]: 
+            continue
+        
+        visited[i] = True
+        picked.append(i)
+
+        find_min(cnt + 1)
+
+        visited[i] = False
+        picked.pop()
+
+visited[0] = True
+picked.append(0)
+find_min(1)
+
+print(ans)
