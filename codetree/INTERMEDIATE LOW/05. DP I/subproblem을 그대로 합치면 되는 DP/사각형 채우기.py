28d803a62cef798326cd5246d388d1243168f431
Jaeseok Park <jaesukpark77@gmail.com>
2023-09-23 19:51:33 +0900

    Add. codetree problem_INTERMEDIATE LOW_05.  DP I_subproblem을 그대로 합치면 되는 DP - 사각형 채우기 problem

---
+++ codetree/INTERMEDIATE LOW/05. DP I/subproblem을 그대로 합치면 되는 DP/사각형 채우기.py
@@ -0,0 +1,14 @@
+# problem link : https://www.codetree.ai/missions/2/problems/rectangle-fill?&utm_source=clipboard&utm_medium=text
+
+MOD = 10007
+
+n = int(input())
+dp = [0] * (n + 1)
+
+dp[0] = 1
+dp[1] = 1
+
+for i in range(2, n + 1):
+    dp[i] = (dp[i - 1] + dp[i - 2]) % MOD
+
+print(dp[n])
