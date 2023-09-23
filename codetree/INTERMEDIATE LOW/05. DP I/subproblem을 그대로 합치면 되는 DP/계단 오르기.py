ee60dddca68a416d6a6e4be4bcd30c809265ce76
Jaeseok Park <jaesukpark77@gmail.com>
2023-09-23 19:43:56 +0900

    Add. codetree problem_INTERMEDIATE LOW_05.  DP I_subproblem을 그대로 합치면 되는 DP - 계단 오르기 problem

---
+++ codetree/INTERMEDIATE LOW/05. DP I/subproblem을 그대로 합치면 되는 DP/계단 오르기.py
@@ -0,0 +1,17 @@
+# problem link : https://www.codetree.ai/missions/2/problems/climbing-stairs?&utm_source=clipboard&utm_medium=text
+
+MAX_N = 1000
+MOD = 10007
+
+n = int(input())
+dp = [0] * (MAX_N + 1)
+
+dp[0] = 1
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
+for i in range(4, n + 1):
+    dp[i] = (dp[i - 2] + dp[i - 3]) % MOD
+
+print(dp[n])
