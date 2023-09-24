6a1dcb2df4137e2cd911ed23dbac8a5e01ead46b
Jaeseok Park <jaesukpark77@gmail.com>
2023-09-24 16:32:27 +0900

    Add. codetree problem_INTERMEDIATE LOW_05.  DP I_격자 안에서 한 칸씩 전진하는 DP - 증가했다가 감소하는 부분 수열 problem

---
+++ codetree/INTERMEDIATE LOW/05. DP I/조건에 맞게 선택적으로 전진하는 DP/증가했다가 감소하는 부분 수열.py
@@ -0,0 +1,28 @@
+# problem link : https://www.codetree.ai/missions/2/problems/increasing-and-descreasing-subsequence?&utm_source=clipboard&utm_medium=text
+
+MAX_K = 2
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [[0] * MAX_K for _ in range(n)]
+
+for i in range(n):
+    dp[i][0] = 1
+    dp[i][1] = 1
+
+    for j in range(i):
+        if arr[j] < arr[i]:
+            dp[i][0] = max(dp[i][0], dp[j][0] + 1)
+
+        if arr[j] > arr[i]:
+            dp[i][1] = max(dp[i][1], dp[j][1] + 1)
+
+
+    dp[i][1] = max(dp[i][1], dp[i][0])
+
+ans = 0
+for i in range(n):
+    for j in range(MAX_K):
+        ans = max(ans, dp[i][j])
+
+print(ans)
