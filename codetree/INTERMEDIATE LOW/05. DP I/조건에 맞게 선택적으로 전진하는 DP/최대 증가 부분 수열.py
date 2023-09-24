39cd76303b4dc2267d5334a73e0350435eb015f8
Jaeseok Park <jaesukpark77@gmail.com>
2023-09-24 15:19:58 +0900

    Add. codetree problem_INTERMEDIATE LOW_05.  DP I_격자 안에서 한 칸씩 전진하는 DP - 최대 증가 부분 수열 problem

---
+++ codetree/INTERMEDIATE LOW/05. DP I/조건에 맞게 선택적으로 전진하는 DP/최대 증가 부분 수열.py
@@ -0,0 +1,13 @@
+# problem link : https://www.codetree.ai/missions/2/problems/longest-increasing-subsequence?&utm_source=clipboard&utm_medium=text
+
+n = int(input())
+
+a = [0] + list(map(int, input().split()))
+dp = [0] * (n + 1)
+
+for i in range(1, n+1):
+    for j in range(i):
+        if a[j] < a[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
