ad5522d7d30463b8cab463089965c87841c7668a
Jaeseok Park <jaesukpark77@gmail.com>
2023-09-25 17:27:53 +0900

    Add. codetree problem_INTERMEDIATE MID_01. 중급 자료구조_HashMap - 원소의 합이 0 problem

---
+++ codetree/INTERMEDIATE MID/01. 중급 자료구조/HashMap/원소의 합이 0.py
@@ -0,0 +1,29 @@
+# problem link : https://www.codetree.ai/missions/8/problems/the-sum-of-the-elements-is-0?&utm_source=clipboard&utm_medium=text
+
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
+D = list(map(int, input().split()))
+
+count = dict()
+
+ans = 0
+
+for i in range(n):
+    for j in range(n):
+        sum_val = A[i] + B[j]
+
+        if sum_val in count:
+            count[sum_val] += 1
+        else:
+            count[sum_val] = 1
+
+for i in range(n):
+    for j in range(n):
+        diff = - C[i] - D[j]
+
+        if diff in count:
+            ans += count[diff]
+
+print(ans)
