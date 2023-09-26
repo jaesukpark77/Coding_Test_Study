ed06a32289c23fa38e72395103fa0b049948468d
Jaeseok Park <jaesukpark77@gmail.com>
2023-09-26 11:00:52 +0900

    Add. codetree problem_INTERMEDIATE MID_01. 중급 자료구조_HashSet - 자리 차지하기 problem

---
+++ codetree/INTERMEDIATE MID/01. 중급 자료구조/TreeSet/차이가 가장 작은 수.py
@@ -0,0 +1,27 @@
+# problem link : https://www.codetree.ai/missions/8/problems/the-number-with-the-smallest-difference?&utm_source=clipboard&utm_medium=text
+
+import math
+from sortedcontainers import SortedSet
+
+INT_MAX = math.inf
+
+n, m = map(int, input().split())
+arr = list(int(input()) for _ in range(n))
+
+s = SortedSet()
+ans = INT_MAX
+
+for x in arr:
+    min_idx = s.bisect_left(m+x)
+
+    if min_idx != len(s):
+        ans = min(ans, s[min_idx] - x)
+
+    max_idx = s.bisect_right(x - m) - 1
+    if max_idx >= 0:
+        ans = min(ans, x - s[max_idx])
+
+if ans == INT_MAX:
+    ans = -1
+
+print(ans)
