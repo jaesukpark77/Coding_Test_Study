67c39fda8129bbf989e4851a32fd04f255cb07b6
Jaeseok Park <jaesukpark77@gmail.com>
2023-09-27 09:30:06 +0900

    Add. codetree problem_INTERMEDIATE MID_02. Shorten Time Technique_Prefix Sum - 부분 수열의 합이 k problem

---
+++ codetree/INTERMEDIATE MID/02. Shorten Time Technique/Prefix Sum/부분 수열의 합이 k.py
@@ -0,0 +1,20 @@
+# problem link : https://www.codetree.ai/missions/8/problems/the-sum-of-the-subsequences-is-k?&utm_source=clipboard&utm_medium=text
+
+n, k = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+prefix_sum = [0] * (n + 1)
+ans = 0
+
+def get_sum(s, e):
+    return prefix_sum[e] - prefix_sum[s - 1]
+
+prefix_sum[0] = 0
+for i in range(1, n+1):
+    prefix_sum[i] = prefix_sum[i - 1] + arr[i]
+
+for i in range(1, n+1):
+    for j in range(i, n+1):
+        if get_sum(i, j) == k:
+            ans += 1
+
+print(ans)
