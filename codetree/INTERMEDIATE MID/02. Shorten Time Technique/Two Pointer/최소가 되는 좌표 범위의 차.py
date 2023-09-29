98da5317f735d9b7ff07d5b70b9fbd9ab4c95145
Jaeseok Park <jaesukpark77@gmail.com>
2023-09-29 11:20:36 +0900

    Add. codetree problem_INTERMEDIATE MID_02. Shorten Time Technique_Two Pointer - 최소가 되는 좌표 범위의 차 problem

---
+++ codetree/INTERMEDIATE MID/02. Shorten Time Technique/Two Pointer/최소가 되는 좌표 범위의 차.py
@@ -0,0 +1,35 @@
+# problem link : https://www.codetree.ai/missions/8/problems/the-minimum-difference-in-coordinate-range?&utm_source=clipboard&utm_medium=text
+
+import math
+from sortedcontainers import SortedSet
+
+n, d = map(int, input().split())
+point = [tuple(map(int, input().split())) for _ in range(n)]
+point = [(0,0)] + sorted(point)
+point_count = SortedSet()
+
+def get_min():
+    if not point_count:return 0
+    return point_count[0][0]
+
+def get_max():
+    if not point_count:return 0
+    return point_count[-1][0]
+
+ans = math.inf
+j = 0
+
+for i in range(1, n+1):
+    while j + 1 <= n and get_max() - get_min() < d:
+        point_count.add((point[j+1][1], point[j+1][0]))
+        j += 1
+
+    if get_max() - get_min() < d:
+        break
+
+    ans = min(ans, point[j][0] - point[i][0])
+
+    point_count.remove((point[i][1], point[i][0]))
+
+if ans == math.inf: print(-1)
+else: print(ans)
