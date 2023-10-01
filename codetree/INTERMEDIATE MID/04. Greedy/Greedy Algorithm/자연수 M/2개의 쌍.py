5f9efc4f16f14918c13f24907153d66dfd44a9e7
Jaeseok Park <jaesukpark77@gmail.com>
2023-10-01 18:36:15 +0900

    Add. codetree problem_INTERMEDIATE MID_04. Greedy_Greedy Algorithm - 2개의 쌍 problem

---
+++ codetree/INTERMEDIATE MID/04. Greedy/Greedy Algorithm/자연수 M/2개의 쌍.py
@@ -0,0 +1,31 @@
+# problem link : https://www.codetree.ai/missions/8/problems/m2-pairs-of-natural-numbers?&utm_source=clipboard&utm_medium=text
+
+n = int(input())
+ans = 0
+
+nums = []
+for _ in range(n):
+    x, y = tuple(map(int, input().split()))
+    nums.append((y, x))
+
+nums.sort()
+
+li, ri = 0, n - 1
+
+while li <= ri:
+    ly, lx = nums[li]
+    ry, rx = nums[ri]
+
+    ans = max(ans, ly + ry)
+
+    if lx < rx:
+        nums[ri] = (ry, rx - lx)
+        li += 1
+    elif lx > rx:
+        nums[li] = (ly, lx - rx)
+        ri -= 1
+    else:
+        li += 1
+        ri -= 1
+
+print(ans)
