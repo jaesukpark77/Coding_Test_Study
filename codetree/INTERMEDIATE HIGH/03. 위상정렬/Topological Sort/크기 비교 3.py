e312b752619441569c3fe3e594700e3f868a0792
Jaeseok Park <jaesukpark77@gmail.com>
2023-10-04 15:47:58 +0900

    Add. codetree problem_INTERMEDIATE HIGH_03. 위상정렬_Topological Sort - 크기 비교 3 problem

---
+++ codetree/INTERMEDIATE HIGH/03. 위상정렬/Topological Sort/크기 비교 3.py
@@ -0,0 +1,31 @@
+# problem link : https://www.codetree.ai/missions/9/problems/size-comparison-3?&utm_source=clipboard&utm_medium=text
+
+import heapq
+
+n, m = map(int, input().split())
+edges = [[] for _ in range(n + 1)]
+
+indegree = [0] * (n+1)
+
+pq = []
+
+for _ in range(m):
+    x, y = tuple(map(int, input().split()))
+
+    edges[x].append(y)
+    indegree[y] += 1
+
+for i in range(1, n+1):
+    if not indegree[i]:
+        heapq.heappush(pq, i)
+
+while pq:
+    x = heapq.heappop(pq)
+
+    print(x, end= ' ')
+
+    for y in edges[x]:
+        indegree[y] -= 1
+
+        if not indegree[y]:
+            heapq.heappush(pq, y)
