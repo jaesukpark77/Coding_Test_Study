3b953e4a9783484020527e0b35a9cf3c92635938
Jaeseok Park <jaesukpark77@gmail.com>
2023-10-04 15:14:45 +0900

    Add. codetree problem_INTERMEDIATE HIGH_03. 위상정렬_Topological Sort - 친구의 키 problem

---
+++ codetree/INTERMEDIATE HIGH/03. 위상정렬/Topological Sort/친구의 키.py
@@ -0,0 +1,30 @@
+# problem link : https://www.codetree.ai/missions/9/problems/height-of-friends?&utm_source=clipboard&utm_medium=text
+
+import sys
+
+sys.setrecursionlimit(100000)
+
+n, m = map(int, input().split())
+edges = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+reversed_order = []
+
+for _ in range(m):
+    x, y = tuple(map(int, input().split()))
+    edges[x].append(y)
+
+def dfs(x):
+    for y in edges[x]:
+        if not visited[y]:
+            visited[y] = True
+            dfs(y)
+
+    reversed_order.append(x)
+
+for i in range(1, n+1):
+    if not visited[i]:
+        visited[i] = True
+        dfs(i)
+
+for num in reversed_order[::-1]:
+    print(num, end=' ')
