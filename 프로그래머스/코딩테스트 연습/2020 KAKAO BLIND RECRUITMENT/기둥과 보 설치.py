5faa0720719b7928094a194a27c59db0466fad06
Jaeseok Park <jaesukpark77@gmail.com>
2023-10-05 14:47:49 +0900

    Add. 프로그래머스 problem_2020 KAKAO BLIND RECRUITMENT - 기둥과 보 설치 problem

---
+++ 프로그래머스/코딩테스트 연습/2020 KAKAO BLIND RECRUITMENT/기둥과 보 설치.py
@@ -0,0 +1,32 @@
+# problem link : https://school.programmers.co.kr/learn/courses/30/lessons/60061
+
+def impossible(result):
+    COL, ROW = 0, 1
+    for x, y, a in result:
+        if a == COL:
+            if y != 0 and (x, y - 1, COL) not in result and (x - 1, y, ROW) not in result and (x, y, ROW) not in result:
+                return True
+        else:
+            if (x, y - 1, COL) not in result and (x + 1, y - 1, COL) not in result and not((x - 1, y, ROW) in result and (x + 1, y, ROW) in result):
+                return True
+    return False
+
+
+def solution(n, build_frame):
+    result = set()
+    
+    for x, y, a, b in build_frame:
+        item = (x, y, a)
+        
+        if b:
+            result.add(item)
+            if impossible(result):
+                result.remove(item)
+        elif item in result:
+            result.remove(item)
+            if impossible(result):
+                result.add(item)
+    
+    answer = list(result)
+    answer.sort(key = lambda x: (x[0], x[1], x[2]))
+    return answer
