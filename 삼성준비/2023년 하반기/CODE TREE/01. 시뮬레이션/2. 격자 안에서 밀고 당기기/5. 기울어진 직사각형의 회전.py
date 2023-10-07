fedb367fc30a6a0c60ce9d1c0979b4f030e8c58d
Jaeseok Park <jaesukpark77@gmail.com>
2023-10-07 19:21:49 +0900

    Add. 삼성준비_2023년 하반기_CODE TREE_01. 시뮬레이션_2. 격자 안에서 밀고 당기기  - 5. 기울어진 직사각형의 회전 problem

---
+++ 삼성준비/2023년 하반기/CODE TREE/01. 시뮬레이션/2. 격자 안에서 밀고 당기기/5. 기울어진 직사각형의 회전.py
@@ -0,0 +1,38 @@
+# problem link : https://www.codetree.ai/missions/2/problems/rotate-slanted-rectangle?&utm_source=clipboard&utm_medium=text
+
+CCW = 0
+CW = 1
+
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+temp = [[0 for _ in range(n)] for _ in range(n)]
+
+def shift(x, y, k, l, move_dir):
+    if move_dir == CCW:
+        dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
+        move_nums = [k, l, k, l]
+    else:
+        dxs, dys = [-1, -1, 1, 1], [-1, 1, 1, -1]
+        move_nums = [l, k, l, k]
+
+    for i in range(n):
+        for j in range(n):
+            temp[i][j] = grid[i][j]
+
+    for dx, dy, move_num in zip(dxs, dys, move_nums):
+        for _ in range(move_num):
+            nx, ny = x + dx, y + dy
+            temp[nx][ny] = grid[x][y]
+            x, y = nx, ny
+    
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] = temp[i][j]
+
+x, y, m1, m2, m3, m4, d = map(int, input().split())
+shift(x - 1, y - 1, m1, m2, d)
+
+for i in range(n):
+    for j in range(n):
+        print(grid[i][j], end=' ')
+    print()
