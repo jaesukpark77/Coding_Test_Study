c498bf9e5436f4af86ac2ae4f18accd3daab413b
Jaeseok Park <jaesukpark77@gmail.com>
2023-10-08 19:57:23 +0900

    Add. 삼성준비_2023년 하반기_CODE TREE_02. 백트래킹_2. N개 중에 M개 고르기(Simple) - 4. 2n개 중에 n개의 숫자를 적절하게 고르기 problem

---
+++ 삼성준비/2023년 하반기/CODE TREE/02. 백트래킹/2. N개 중에 M개 고르기(Simple)/4. 2n개 중에 n개의 숫자를 적절하게 고르기.py
@@ -0,0 +1,44 @@
+# proeblem link : https://www.codetree.ai/missions/2/problems/choose-n-out-of-2n-properly?&utm_source=clipboard&utm_medium=text
+
+import sys
+
+INT_MAX = sys.maxsize
+
+# 변수 선언 및 입력:
+
+n = int(input())
+num = list(map(int, input().split()))
+visited = [False for _ in range(2 * n)]
+
+ans = INT_MAX
+
+
+def calc():
+    diff = 0
+    for i in range(2 * n):
+        diff = (diff + num[i]) if visited[i] else diff - num[i]
+    
+    return abs(diff)
+
+
+def find_min(idx, cnt):
+    global ans
+    
+    if cnt == n:
+        ans = min(ans, calc())
+        return
+    
+    if idx == 2 * n:
+        return
+    
+    # 현재 숫자를 첫 번째 그룹에 사용한 경우입니다.
+    visited[idx] = True
+    find_min(idx + 1, cnt + 1)
+    visited[idx] = False
+    
+    # 현재 숫자를 두 번째 그룹에 사용한 경우입니다.
+    find_min(idx + 1, cnt)
+    
+
+find_min(0, 0)
+print(ans)
