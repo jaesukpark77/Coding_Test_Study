f5298a1fc0bc4b26d3fedbd37b706c6c5459bd96
Jaeseok Park <jaesukpark77@gmail.com>
2023-10-09 19:59:44 +0900

    Add. 삼성준비_2023년 하반기_CODE TREE_04. 시간 최적화_3. Priority Queue - 10. 배열 추출 2 problem

---
+++ 삼성준비/2023년 하반기/CODE TREE/04. 시간 최적화/3. Priority Queue/10. 배열 추출 2.py
@@ -0,0 +1,18 @@
+# problem link : https://www.codetree.ai/missions/8/problems/array-extraction-2?&utm_source=clipboard&utm_medium=text
+
+import heapq
+
+n = int(input())
+pq = []
+
+for _ in range(n):
+    x = int(input())
+
+    if x != 0:
+        heapq.heappush(pq, (abs(x), x))
+    else:
+        if not pq:
+            print(0)
+            continue
+        _, v = heapq.heappop(pq)
+        print(v)
