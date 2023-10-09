5c2f91746a1299b7497c459fbc6fa3c70755f74a
Jaeseok Park <jaesukpark77@gmail.com>
2023-10-09 09:51:00 +0900

    Add. 삼성준비_2023년 하반기_CODE TREE_03. 그래프 탐색_2. BFS 탐색 - 4. 돌 잘 치우기 problem

---
+++ 삼성준비/2023년 하반기/CODE TREE/03. 그래프 탐색/2. BFS 탐색/4. 돌 잘 치우기.py
@@ -0,0 +1,83 @@
+# problem link : https://www.codetree.ai/missions/2/problems/clear-stones-well?&utm_source=clipboard&utm_medium=text
+
+from collections import deque
+
+n, k, m = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+
+ans = 0
+s_pos = []
+stone_pos = [(i, j) for i in range(n) for j in range(n) if a[i][j] == 1]
+selected_stones = []
+
+q = deque()
+visited = [[False] * n for _ in range(n)]
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+def can_go(x, y):
+    return in_range(x, y) and not a[x][y] and not visited[x][y]
+
+def bfs():
+    while q:
+        x, y = q.popleft()
+
+        dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+
+            if can_go(nx, ny):
+                q.append((nx, ny))
+                visited[nx][ny] = True
+
+def initailze_visited():
+    for i in range(n):
+        for j in range(n):
+            visited[i][j] = False
+
+def calc():
+    for x, y in selected_stones:
+        a[x][y] = 0
+    
+    initailze_visited()
+
+    for x, y in s_pos:
+        q.append((x, y))
+        visited[x][y] = True
+
+    bfs()
+
+    for x, y in selected_stones:
+        a[x][y] = 1
+
+    cnt = 0
+
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j]:
+                cnt += 1
+
+    return cnt
+
+def find_max(idx, cnt):
+    global ans
+
+    if idx == len(stone_pos):
+        if cnt == m:
+            ans = max(ans, calc())
+        return
+
+    selected_stones.append(stone_pos[idx])
+    find_max(idx + 1, cnt + 1)
+    selected_stones.pop()
+
+    find_max(idx + 1, cnt)    
+
+for _ in range(k):
+    r, c = tuple(map(int, input().split()))
+    s_pos.append((r-1, c-1))
+
+find_max(0, 0)
+print(ans)
