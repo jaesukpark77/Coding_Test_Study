b5f607d74a7c2edb87b375ec63343821b7a54d56
Jaeseok Park <jaesukpark77@gmail.com>
2023-10-19 15:51:23 +0900

    Add. 프로그래머스_고득점 Kit_동적계획법(Dynamic Programming) - 정수 삼각형(Solution 2) problem

--- 프로그래머스/코딩테스트 연습/고득점 Kit/동적계획법(Dynamic Programming)/정수 삼각형.py
+++ 프로그래머스/코딩테스트 연습/고득점 Kit/동적계획법(Dynamic Programming)/정수 삼각형.py
@@ -1,5 +1,7 @@
 # problem link : https://school.programmers.co.kr/learn/courses/30/lessons/43105
 
+# solution 1
+
 def solution(triangle):
     leng = len(triangle)
     dp = [[0] * leng for _ in range(leng)]
@@ -11,3 +13,14 @@ def solution(triangle):
             dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j] + triangle[i+1][j+1])
     
     return max(dp[-1])
+
+# solution 2
+
+def solution(triangle):
+    # 뒤에서부터 역순으로 DP 테이블 초기화
+    for i in range(len(triangle) - 2, -1, -1):
+        for j in range(len(triangle[i])):
+            # 아래층의 두 수 중 큰 값을 현재 칸의 값에 더함
+            triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1])
+
+    return triangle[0][0]
